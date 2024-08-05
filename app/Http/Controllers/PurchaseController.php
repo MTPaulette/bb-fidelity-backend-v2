6f2261e69d7dbfedc92f4d68415da6d988b86079
MTPaulette <mayoguepaulette@gmail.com>
2024-08-05 10:00:32 +0100

    ready ro deploy

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -6,6 +6,7 @@ use App\Models\Service;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cookie;
 
 class PurchaseController extends Controller
 {
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,9 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
+window.axios.defaults.withCredentials = true;
+//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
